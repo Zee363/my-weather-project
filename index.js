2741b84d45c2ac9a06aae85375419d27fc392af1
Zee363 <133252086+Zee363@users.noreply.github.com>
2023-11-13 11:54:26 +0200

    Added the API integration

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function findCity(city) {
+  let apiKey = "4b230af38fdc2b34b014a5b47d0e5tob";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function submitSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   console.log(searchInput.value);
-let cityElement = document.querySelector("#city");
-cityElement.innerHTML = searchInput.value;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+  findCity(searchInput.value);
 }
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", submitSearch);
+
+findCity("Cape Town");
