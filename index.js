f9a7b043df886f7ce6e9d5c9de04615f7a7aa0a4
Zee363 <133252086+Zee363@users.noreply.github.com>
2023-11-13 12:54:55 +0200

    Added more weather data

--- index.js
+++ index.js
@@ -2,9 +2,35 @@ function updateWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let speedElement = document.querySelector("#speed-of-wind");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
-  cityElement.innerHTML = response.data.city;
+
+  cityElement.innerHTML = response.data.city;temperature
+  timeElement.innerHTML = formattedDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  speedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function formattedDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let day = days[date.getDay()];
+   
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
+
+  
+
 }
 
 function findCity(city) {
